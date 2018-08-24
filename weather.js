e27542aa242b2d5661d7849190e729e3a879929e
Milo MacPhail <milomacphail@gmail.com>
2018-08-24 00:10:01 +0000

    Kinda have it

--- weather.js
+++ weather.js
@@ -1,11 +1,64 @@
-if (navigator.geolocation) {
-            window.onload = function(){
-       var currentPosition;
-       function getCurrentLocation (position){
-           currentPosition =  position;
-           latitude = currentPosition.coords.latitude;
-           longitude = currentPosition.coords.longitude;
-      
-       $.getJSON("https://fcc-weather-api.glitch.me//api/current?lon=:longitude&lat=:latitude")
+const loc = document.getElementById("location");
+const temNum = document.getElementById("temperature-num");
+const temScale = document.getElementById("temperature-scale");
+const weatherCon = document.getElementById("weather-condition");
+const weatherIcon = document.getElementById("weather-icon");
+
+
+// get location
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      getWeather(position.coords.latitude, position.coords.longitude);
+    });
+  } else {
+    loc.innerHTML = "Geolocation is not supported by this browser.";
+  }
 }
+
+// get weather data according to the location
+function getWeather(lat, long) {
+  const root = "https://fcc-weather-api.glitch.me/api/current?";
+  fetch(`${root}lat=${lat}&lon=${long}`, { method: "get" })
+    .then(resp => resp.json())
+    .then(data => {
+      updateDataToUI(data.name, data.weather, data.main.temp);
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+}
+
+// update the data from API to DOM
+function updateDataToUI(location, weather, temp) {
+  weatherIcon.innerHTML = `<img src="${weather[0].icon}" />`;
+  weatherCon.innerHTML = weather[0].main;
+  loc.innerHTML = location;
+  temNum.innerHTML = `${temp}`;
+}
+
+window.onload = function() {
+  getLocation();
 };
+
+// helper function change from C to F
+function cToF(celsius) {
+  return celsius * 9 / 5 + 32;
+}
+// helper function: change from F to C
+function fToC(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+function toggleScale() {
+  if (temScale.innerHTML === "C") {
+    temNum.innerHTML = cToF(temNum.innerHTML).toFixed(2);
+    temScale.innerHTML = "F";
+  } else if (temScale.innerHTML === 'F') {
+    temNum.innerHTML = fToC(temNum.innerHTML).toFixed(2);
+    temScale.innerHTML = "C";
+  }
+}
+
+// toggle the temperature scale
+temScale.addEventListener("click", toggleScale);
