c223a3e8cebe986b79b6bb34dbc021a38946ac34
Milo MacPhail <milomacphail@gmail.com>
2018-08-25 17:32:41 +0000

    Functionality complete

--- weather.js
+++ weather.js
@@ -1,11 +1,10 @@
 const loc = document.getElementById("location");
 const temNum = document.getElementById("temperature-num");
-const temScale = document.getElementById("temperature-scale");
+const tempScale = document.getElementById("temperature-scale");
 const weatherCon = document.getElementById("weather-condition");
 const weatherIcon = document.getElementById("weather-icon");
 
 
-// get location
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -16,10 +15,9 @@ function getLocation() {
   }
 }
 
-// get weather data according to the location
 function getWeather(lat, long) {
   const root = "https://fcc-weather-api.glitch.me/api/current?";
-  fetch(`${root}lat=${lat}&lon=${long}`, { method: "get" })
+  fetch(`${root}lat=${lat}&lon=${long}`)
     .then(resp => resp.json())
     .then(data => {
       updateDataToUI(data.name, data.weather, data.main.temp);
@@ -29,36 +27,33 @@ function getWeather(lat, long) {
     });
 }
 
-// update the data from API to DOM
 function updateDataToUI(location, weather, temp) {
   weatherIcon.innerHTML = `<img src="${weather[0].icon}" />`;
   weatherCon.innerHTML = weather[0].main;
   loc.innerHTML = location;
-  temNum.innerHTML = `${temp}`;
+  temNum.innerHTML = `${temp.toFixed()}`;
 }
 
 window.onload = function() {
   getLocation();
 };
 
-// helper function change from C to F
 function cToF(celsius) {
   return celsius * 9 / 5 + 32;
 }
-// helper function: change from F to C
+
 function fToC(fahrenheit) {
   return (fahrenheit - 32) * 5 / 9;
 }
 
 function toggleScale() {
-  if (temScale.innerHTML === "C") {
-    temNum.innerHTML = cToF(temNum.innerHTML).toFixed(2);
-    temScale.innerHTML = "F";
-  } else if (temScale.innerHTML === 'F') {
-    temNum.innerHTML = fToC(temNum.innerHTML).toFixed(2);
-    temScale.innerHTML = "C";
+  if (tempScale.innerHTML === "C") {
+    temNum.innerHTML = cToF(temNum.innerHTML).toFixed(0);
+    tempScale.innerHTML = "F";
+  } else if (tempScale.innerHTML === 'F') {
+    temNum.innerHTML = fToC(temNum.innerHTML).toFixed(0);
+    tempScale.innerHTML = "C";
   }
 }
 
-// toggle the temperature scale
-temScale.addEventListener("click", toggleScale);
+tempScale.addEventListener("click", toggleScale);
